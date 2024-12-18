fd11a94b1de2bc396fcc7b988dbb9f3488a4a200
vladimir-seneos <168069917+vladimir-seneos@users.noreply.github.com>
2024-12-18 12:07:45 +0200

    8th commit

--- lambda/main.py
+++ lambda/main.py
@@ -2,6 +2,6 @@ def handler(event, context):
     print(event)
     response_body = {
         "message": "Hello World!!!!",
-        "version": "1.0.0"
+        "version": "1.0.1"
     }
     return{"statusCode":200, "body":response_body}
